23ad8da0cf58554a97f33327fc670649c4fa6370
izumin5210 <masayuki@izumin.info>
2017-06-14 12:10:59 +0900

    bundle gem .

---
+++ lib/rspec/cheese.rb
@@ -0,0 +1,7 @@
+require "rspec/cheese/version"
+
+module Rspec
+  module Cheese
+    # Your code goes here...
+  end
+end
---
+++ lib/rspec/cheese/version.rb
@@ -0,0 +1,5 @@
+module Rspec
+  module Cheese
+    VERSION = "0.1.0"
+  end
+end
