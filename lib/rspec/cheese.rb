80005a876f7dd020d42750fc7452ef8015a6c1be
izumin5210 <masayuki@izumin.info>
2017-06-16 17:59:10 +0900

    Refactor

--- lib/rspec/cheese.rb
+++ lib/rspec/cheese.rb
@@ -1,11 +1,20 @@
 require "rspec/cheese/version"
-require "rspec/cheese/snapshot"
+require "rspec/cheese/manager"
 require "rspec/cheese/matchers"
+require "rspec/cheese/snap_file"
+require "rspec/cheese/snapshot"
 
 module RSpec
   module Cheese
-    RSpec.configure do |config|
-      config.include Matchers
-    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include RSpec::Cheese::Matchers
+
+  config.add_setting RSpec::Cheese::Manager.settings_name, default: RSpec::Cheese::Manager.new
+
+  config.after :suite do
+    # TODO: update snapshot if needed
   end
 end
--- lib/rspec/cheese/snapshot.rb
+++ lib/rspec/cheese/snapshot.rb
@@ -3,88 +3,28 @@ require "yaml"
 module RSpec
   module Cheese
     class Snapshot
-      # TODO: Confugurable
-      SNAPSHOTS_DIRNAME = "__snapshots__"
-      SNAPSHOTS_FILE_EXT = ".yml.snap"
+      attr_reader :example, :data, :updated_content
+      attr_accessor :expected, :actual
 
-      class << self
-        def create(example:)
-          Snapshot.new(example).tap do |snapshot|
-            snapshot.create_snapshots_file_if_needed
-            snapshot.load
-          end
-        end
-      end
-
-      attr_reader :example
-
-      def initialize(example)
+      # @param [RSpec::Core::Example] example The example
+      def initialize example
         @example = example
-        @added = false
       end
 
-      def load
-        @yaml = YAML.load(File.read(snapshots_path)) || {}
+      # @return [boolean] true if the snapshot is stored
+      def new?
+        expected.nil?
       end
 
-      def data
-        @yaml[key]
+      # @return [boolean] true if the snapshot changed
+      def changed?
+        actual != expected
       end
 
-      def added?
-        @added
-      end
-
-      def create_snapshots_file_if_needed
-        unless snapshots_exists?
-          create_snapshots_file
-          @added = true
-        end
-      end
-
-      def save(content)
-        if added?
-          @yaml[key] = content
-          File.write(snapshots_path, YAML.dump(@yaml))
-        end
-      end
-
+      # @return [String] key The snapshot key
       def key
         # TODO: Support 1-testcase n-epectation
         example.id
-      end
-
-      def spec_filename
-        File.basename(spec_path)
-      end
-
-      def snapshots_filename
-        "#{spec_filename}#{SNAPSHOTS_FILE_EXT}"
-      end
-
-      def spec_path
-        @spec_path = File.expand_path(example.file_path)
-      end
-
-      def snapshots_dirname
-        @difname = File.join(File.dirname(spec_path), SNAPSHOTS_DIRNAME)
-      end
-
-      def snapshots_path
-        @file_path = File.join(snapshots_dirname, snapshots_filename)
-      end
-
-      def snapshots_exists?
-        File.exist?(snapshots_path)
-      end
-
-      private
-
-      def create_snapshots_file
-        unless File.exist? snapshots_dirname
-          FileUtils.mkdir snapshots_dirname
-        end
-        FileUtils.touch snapshots_path
       end
     end
   end
