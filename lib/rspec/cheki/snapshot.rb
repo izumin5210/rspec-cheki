93a8b9827ce5bf5b0774cb0a0eb145213510bc75
izumin5210 <masayuki@izumin.info>
2017-06-18 02:03:09 +0900

    Refactor

--- lib/rspec/cheki/snapshot.rb
+++ lib/rspec/cheki/snapshot.rb
@@ -3,22 +3,38 @@ require "yaml"
 module RSpec
   module Cheki
     class Snapshot
-      attr_reader :key
-      attr_accessor :expected, :actual
+      attr_reader :key, :expected, :actual
 
       # @param [String] key The key
       def initialize key
         @key = key
+        @expected_updated = false
+        @actual_updated = false
+      end
+
+      # @return [boolean] true if the actual value is equal to stored value from snapshot
+      def match?
+        actual == expected
       end
 
       # @return [boolean] true if the snapshot is stored
       def new?
-        expected.nil?
+        !@expected_updated
       end
 
       # @return [boolean] true if the snapshot changed
-      def changed?
-        actual != expected
+      def needs_update?
+        @actual_updated && !match?
+      end
+
+      def expected= value
+        @expected_updated = true
+        @expected = value
+      end
+
+      def actual= value
+        @actual_updated = true
+        @actual = value
       end
     end
   end
